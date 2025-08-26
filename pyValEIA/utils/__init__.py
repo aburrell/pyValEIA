20af08f3188554e6b0b75e7bbe84ee364031d553
Angeline G. Burrell <aburrell@users.noreply.github.com>
2025-08-26 15:23:44 -0400

    ENH: added coordinate submodule
    Created a `utils` module with a coordinate sub-module and functions inside.

---
+++ pyValEIA/utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
+"""Submodule for converting values and other supporting utilities."""
+
+# Import the package modules and top-level classes
+from pyValEIA.utils import coords  # noqa F401
