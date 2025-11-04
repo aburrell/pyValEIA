36e476faa62c3902901f298e520fe4f2fce18ad6
Angeline G. Burrell <aburrell@users.noreply.github.com>
2025-11-04 16:59:29 -0500

    ENH: added a cleaning sub-module
    Added a sub-module for data cleaning functions.

--- pyValEIA/utils/__init__.py
+++ pyValEIA/utils/__init__.py
@@ -5,6 +5,7 @@
 """Submodule for converting values and other supporting utilities."""
 
 # Import the package modules and top-level classes
+from pyValEIA.utils import clean  # noqa F401
 from pyValEIA.utils import coords  # noqa F401
 from pyValEIA.utils import conjunctions  # noqa F401
 from pyValEIA.utils import filters  # noqa F401
