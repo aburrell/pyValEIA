5e4104d7408bd60bc8b07c26847f256f3a8e28a3
alanahco <alanahco@umich.edu>
2026-02-26 17:07:51 -0500

    Bug fix zero_max
    Updated maxes to be consistent with NoneType instead of length greater than 0.

--- pyValEIA/eia/detection.py
+++ pyValEIA/eia/detection.py
@@ -993,7 +993,7 @@ def zero_max(lat, dens, zlats, maxes=None):
                 # If it is a peak, set the second index
                 p2 = abs(lat - latz_north[tn]).argmin()
 
-    if len(maxes) > 0:
+    if maxes is not None:
         # If one peak is given, replace either p1 (souther) or p2 (northern)
         if lat[maxes[0]] > 0:
             p2 = maxes[0][0]
