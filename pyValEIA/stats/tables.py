1c68cf47b45dc7055282fb6250b6075c5da8917f
Angeline G. Burrell <aburrell@users.noreply.github.com>
2026-08-27 12:10:06 -0400

    BUG: fixed bugs in table functions
    Fixed bugs in table functions by:
    - replacing hard-coded variables with input references,
    - adding input error catches,
    - fixing the function names called,
    - removing unneeded inputs, and
    - using input to determine number of rows in formatting the decision table.

--- pyValEIA/stats/tables.py
+++ pyValEIA/stats/tables.py
@@ -48,15 +48,15 @@ def decision_table_sat(states, sats=None, sat_key='Sat', eia_type='eia',
     """
     # If not provided, get the unique satellite IDs
     if sats is None:
-        sats = np.unique(states['Sat'].values)
+        sats = np.unique(states[sat_key].values)
 
     # Cycle over each unique satellite
     for i, s in enumerate(sats):
         # Sum total HMFCs
-        hit = sum(states['skill'][states['Sat'] == s] == 'H')
-        falarm = sum(states['skill'][states['Sat'] == s] == 'F')
-        miss = sum(states['skill'][states['Sat'] == s] == 'M')
-        corneg = sum(states['skill'][states['Sat'] == s] == 'C')
+        hit = sum(states['skill'][states[sat_key] == s] == 'H')
+        falarm = sum(states['skill'][states[sat_key] == s] == 'F')
+        miss = sum(states['skill'][states[sat_key] == s] == 'M')
+        corneg = sum(states['skill'][states[sat_key] == s] == 'C')
 
         # Initialize or update the output table
         sat_name = ' '.join([const_name, s])
@@ -76,17 +76,13 @@ def decision_table_sat(states, sats=None, sat_key='Sat', eia_type='eia',
     return table_frame
 
 
-def style_df_table(df_table, eia_type, sat_names=None):
+def style_df_table(df_table):
     """Style decision table.
 
     Parameters
     ----------
     df_table : pd.DataFrame
         DataFrame created by decision_table_sat
-    eia_type : str
-        string designating which eia type is being reported
-    sat_names : list-like or NoneType
-        List of satellite names in `df_table` or None to use Swarm defaults.
 
     Returns
     -------
@@ -96,11 +92,24 @@ def style_df_table(df_table, eia_type, sat_names=None):
     Raises
     ------
     ValueError
-        For unknown constellation name
+        For more than two state options or more than two MultiIndex levels
 
     """
-    if sat_names is None:
-        sat_names = ['Swarm A', 'Swarm B', 'Swarm C']
+    # Assert there are two levels and the second level has two values
+    if len(df_table.index.levels) != 2:
+        raise ValueError(
+            'unknown input, expecting a MultiIndex with two levels')
+
+    if len(df_table.index.levels[1]) != 2:
+        raise ValueError('unknown input, expecting a state with two options')
+
+    # Get the number of instruments
+    ninst = len(df_table.index.levels[0])
+
+    # Build a list of cells
+    cell_list = list()
+    for i in range(ninst):
+        cell_list.extend([['true ', 'false '], ['false ', 'true ']])
 
     # Initialize the output
     styled_frame = df_table.style.format('{:.0f}')
@@ -110,22 +119,16 @@ def style_df_table(df_table, eia_type, sat_names=None):
         {'selector': '.true', 'props': 'background-color: #e6ffe6;'},
         {'selector': '.false', 'props': 'background-color: #ffe6e6;'},],
         overwrite=False)
-    cell_color = pd.DataFrame([['true ', 'false '],
-                               ['false ', 'true '],
-                               ['true ', 'false '],
-                               ['false ', 'true '],
-                               ['true ', 'false '],
-                               ['false ', 'true ']],
-                              index=df_table.index,
+    cell_color = pd.DataFrame(cell_list, index=df_table.index,
                               columns=df_table.columns[:len(df_table)])
 
     # Add borders
-    for l0 in sat_names:
+    for l0 in df_table.index.levels[0]:
         styled_frame.set_table_styles(
-            {(l0, 'Non-' + eia_type):
+            {(l0, df_table.index.levels[1][0]):
              [{'selector': '', 'props':
                'border-bottom: 2px solid black;'}],
-             (l0, eia_type):
+             (l0, df_table.index.levels[1][1]):
              [{'selector': '.level0', 'props':
                'border-bottom: 2px solid black'}]},
             overwrite=False, axis=1)
@@ -177,15 +180,15 @@ def lss_table_sat(model1, model2, model1_name='Model1', model2_name='Model2',
     """
     # If not provided, get the unique satellite IDs from the first DataFrame
     if sats is None:
-        sats = np.unique(model1['Sat'].values)
+        sats = np.unique(model1[sat_key].values)
 
     # Cycle through all the desired satellites
     for i, s in enumerate(sats):
         # Calculate the Liemohn skill scores
-        lss1_m1, lss2_m1, lss3_m1, lss4_m1 = skill_score.Liemohn_Skill_Scores(
-            model1['skill'][model1['Sat'] == s])
-        lss1_m2, lss2_m2, lss3_m2, lss4_m2 = skill_score.Liemohn_Skill_Scores(
-            model2['skill'][model2['Sat'] == s])
+        lss1_m1, lss2_m1, lss3_m1, lss4_m1 = skill_score.liemohn_skill_score(
+            model1['skill'][model1[sat_key] == s])
+        lss1_m2, lss2_m2, lss3_m2, lss4_m2 = skill_score.liemohn_skill_score(
+            model2['skill'][model2[sat_key] == s])
 
         # Initalize or update the output table
         sat_name = ' '.join([const_name, s])
@@ -194,7 +197,7 @@ def lss_table_sat(model1, model2, model1_name='Model1', model2_name='Model2',
                 [[lss1_m1, lss1_m2], [lss2_m1, lss2_m2],
                  [lss3_m1, lss3_m2], [lss4_m1, lss4_m2]],
                 index=pd.MultiIndex.from_product(
-                    [[sat_name], ['lss1', 'lss2', 'lss3', 'lss4']]),
+                    [[sat_name], ['LSS1', 'LSS2', 'LSS3', 'LSS4']]),
                 columns=[model1_name, model2_name])
         else:
             lss_df.loc[(sat_name, 'LSS1'), :] = np.array([lss1_m1, lss1_m2])
@@ -214,8 +217,6 @@ def style_lss_table(lss_df, sat_names=None):
     ----------
     lss_df : pd.DataFrame
         DataFrame created by lss_table_sat
-    sat_names : list-like or NoneType
-        List of satellite names in `lss_df` or None to use Swarm defaults.
 
     Returns
     -------
@@ -223,16 +224,20 @@ def style_lss_table(lss_df, sat_names=None):
         LSS table with dividers
 
     """
-    if sat_names is None:
-        sat_names = ['Swarm A', 'Swarm B', 'Swarm C']
+    # Assert there are two levels
+    if len(lss_df.index.levels) != 2:
+        raise ValueError(
+            'unknown input, expecting a MultiIndex with two levels')
 
+    # Initalize the output
     styled_table = lss_df.style.format()
 
-    for l0 in sat_names:
+    # Add borders
+    for l0 in lss_df.index.levels[0]:
         styled_table.set_table_styles(
-            {(l0, 'LSS4'): [{'selector': '', 'props':
+            {(l0, lss_df.index.levels[1][-1]): [{'selector': '', 'props':
                              'border-bottom: 2px solid black;'}],
-             (l0, 'LSS1'): [{'selector': '.level0', 'props':
+             (l0, lss_df.index.levels[1][0]): [{'selector': '.level0', 'props':
                              'border-bottom: 2px solid black'}]},
             overwrite=False, axis=1)
 
